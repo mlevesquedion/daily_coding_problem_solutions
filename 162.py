4f8052588af837a2cf178f3b94a50e68b86466f4
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-02-18 17:58:36 -0500

    162.0

---
+++ 162.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+
+def group(words, prefix_size):
+    groups = defaultdict(list)
+    for w in words:
+        groups[w[:prefix_size]].append(w)
+    return groups.values()
+
+
+def solve(words):
+    results = []
+    prefix_size = 1
+    groups = group(words, prefix_size)
+    while groups:
+        words = []
+        for g in groups:
+            if len(g) == 1:
+                results.append(g[0][:prefix_size])
+            else:
+                words += g
+        prefix_size += 1
+        groups = group(words, prefix_size)
+    return results
+
+
+if __name__ == "__main__":
+    words = 'dog cat car apple apricot fish'.split()
+    print(solve(words))
