f3701c8031529b570c9313f0dd1421facce1ed2f
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-04-03 12:07:15 -0400

    206.1

--- 206.hs
+++ 206.hs
@@ -1,4 +1,9 @@
+import qualified Data.Map   as Map
+import           Data.Maybe (maybe)
+
 solve :: [String] -> [Int] -> [String]
-solve xs = map (uncurry (!!)) . zip (repeat xs)
+solve xs = map (maybe "" id . flip Map.lookup m)
+  where
+    m = Map.fromList (zip [0 ..] xs)
 
 main = putStrLn . show $ solve ["a", "b", "c"] [2, 1, 0]
