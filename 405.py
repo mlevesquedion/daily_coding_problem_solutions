77175c1e558ac780e2f61e59930b42b9b050f50c
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-12-29 12:50:17 -0500

    405

---
+++ 405.py
@@ -0,0 +1,11 @@
+def solve(tree):
+    if tree is None:
+        return 0
+    if tree.left is None and tree.right is None:
+        return 1
+    if (tree.left and tree.left.val < tree.val) or (
+        tree.right and tree.right.val > tree.val
+    ):
+        return 1 + solve(tree.left) + solve(tree.right)
+    else:
+        return max(solve(tree.left), solve(tree.right))
