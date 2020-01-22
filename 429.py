6c002ca667187f025a07d9c6434d8310ab6780dc
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2020-01-22 15:42:09 -0500

    429 - small tweak for efficiency

--- 429.py
+++ 429.py
@@ -2,10 +2,12 @@ from itertools import chain
 
 
 def pascal(k):
-    pascal = [0, 1, 0]
+    pascal = [1, 0]
     for i in range(1, k):
-        pascal = [0] + [pascal[j] + pascal[j + 1] for j in range(i + 1)] + [0]
-    return pascal[1:-1]
+        pascal = [
+            pascal[(j - 1) % (i + 1)] + pascal[j] for j in range(i + 1)
+        ] + [0]
+    return pascal[:-1]
 
 
 assert pascal(5) == [1, 4, 6, 4, 1]
