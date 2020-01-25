ed50556d79ba0c499d2c027bed6ca6419026ad6e
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2020-01-24 22:52:11 -0500

    trim unnecessary modulus

--- 429.py
+++ 429.py
@@ -4,9 +4,7 @@ from itertools import chain
 def pascal(k):
     pascal = [1, 0]
     for i in range(1, k):
-        pascal = [
-            pascal[(j - 1) % (i + 1)] + pascal[j] for j in range(i + 1)
-        ] + [0]
+        pascal = [pascal[j - 1] + pascal[j] for j in range(i + 1)] + [0]
     return pascal[:-1]
 
 
