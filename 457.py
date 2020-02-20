7f94ec4a2b7232b7422ca4b804f570956e02f82f
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2020-02-20 16:54:15 -0500

    457 - optimized

--- 457.py
+++ 457.py
@@ -15,6 +15,37 @@ def solve(S, W):
         counts[S[i + len(W)]] += 1
     return positions
 
+# optimized, O(|S|)
+# the main idea is we only need to keep track of the difference between
+# the counts and the reference.
+# if the diffs are all zero, then we match the reference exactly
+def solve(S, W):
+    reference = Counter(W)
+    diff = Counter(S[:len(W)]) - reference
+    n_zeroes = len(reference) - len(diff)
+
+    positions = []
+    for i in range(len(S) - len(W) + 1):
+        if n_zeroes == len(reference):
+            positions.append(i)
+
+        if i == len(S) - len(W):
+            break
+
+        if diff[S[i]] == 0 and S[i] in reference:
+            n_zeroes -= 1
+        diff[S[i]] -= 1
+        if diff[S[i]] == 0 and S[i] in reference:
+            n_zeroes += 1
+
+        if diff[S[i + len(W)]] == 0 and S[i + len(W)] in reference:
+            n_zeroes -= 1
+        diff[S[i + len(W)]] += 1
+        if diff[S[i + len(W)]] == 0 and S[i + len(W)] in reference:
+            n_zeroes += 1
+
+    return positions
+
 
 assert solve("abxaba", "ab") == [0, 3, 4]
 
